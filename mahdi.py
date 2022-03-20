8ebe876e801510926b2c156d31736e45f0cb8799
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-20 17:53:28 +0600

    Update mahdi.py

--- mahdi.py
+++ mahdi.py
@@ -76,11 +76,7 @@ pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
 if pil in ["01","1"]:
 		os.system('git clone https://github.com/Shuvo-BBHH/SYLHET1')
 		os.system('cd SYLHET1')
-		os.system('python mahdi.py')
-		time.sleep(2)
-		print (" ")
-		n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-		time.sleep(2)
+		os.system('python mahdi.py')		
 		main()
 
 elif pil in ["02","2"]:
