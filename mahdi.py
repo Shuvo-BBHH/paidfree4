3a374033128cccd82d5d4c37257f1f600a22e43d
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-20 11:28:39 +0600

    Create mahdi.py

---
+++ mahdi.py
@@ -0,0 +1,116 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+os.system("pip install requests")
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+green="\033[0;32m"        # Green
+
+color_off="\033[0m"       # Text Reset
+
+bblack="\033[1;30m"       # Black
+
+bred="\033[1;31m"         # Red
+
+ured="\033[4;31m"         # Red
+
+on_green="\033[42m"       # Green
+
+blue="\033[0;34m"         # Blue
+
+lightblue = '\033[94m'
+
+red = '\033[91m'
+
+white = '\33[97m'
+
+yellow = '\33[93m'
+
+green = '\033[1;32m'
+
+cyan  = "\033[96m"
+
+end = '\033[0m'
+
+purple="\033[0;35m"
+
+def main():
+	os.system("clear")
+print("""\033[0;45m.88b  d88.  .d8b.  db   db d8888b. d888888b\n88'YbdP`88 d8' `8b 88   88 88  `8D   `88'\n88  88  88 88ooo88 88ooo88 88   88    88\n88  88  88 88~~~88 88~~~88 88   88    88\n88  88  88 88   88 88   88 88  .8D   .88.\nYP  YP  YP YP   YP YP   YP Y8888D' Y888888P                                             
+\033[0m================================================================
+  \33[93mAUTHOR :\033[91m[MAHDI HASAN] SHUVO
+   \033[0;33mGITHUB : \033[1;97mhttps://github.com/Shuvo-BBHH
+   WELL COME SYLHET GAMG (IAM NEW MEMBER IN YOUR GRUP)
+  LIVE in Sylhet (Read in class 10)
+\033[42mNo NEED GF \033[0;31mIF YOU LOVE ME I LOVE YOU IF U HAT ME I FUCK YOU 
+================================================================""")
+     	
+print("""
+\033[42m[1] CLONE FROM 2009 ID
+\033[0;88m[2]CLONE FROM 2006-2009 ID 
+\033[0;88m[3]CLONE FROM 2010-2020 ID
+\033[0;33m[4]CLONE FROM  2014-2021 ID
+\033[0;32m[5] CLONE FROM INSTRAGAM ID
+\033[0;33m[6]CLONE FROM FRIENDLIST (ND TOKEN)
+\033[42m[7]CLONE FROM  PUBLICK ID  (ujst now login)
+\033[0;33m[8]CLONE FROM ID BANGLADESH 11DIG
+\033[0;33m[9]CLONE FROM NUMBER BD
+\033[0;88m[10]CLONE FROM FREOM PAKISTAN 
+\033[1;88m[11]CLONE FROM FROM INDIA
+\033[0;33m[12]CLONE FROM 2009-2010
+""")
+pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
+
+if pil in ["01","1"]:
+	try: 
+		    os.system('pkg install python2 -y')
+                   
+                    
+                   
+	
+elif pil in ["02","2"]:
+	try: 
+                     os.system('rm -rf FLAME-2K9')
+                      git clone https://github.com/Naim75o/FLAME-2K9
+                      cd FLAME-2K9
+                      python2 NAIM-2K9.py
+                      main()
+		
+elif pil in ["02","2"]:
+    	try: 
+	           os.system('git clone https://github.com/Shuvo-BBHH/mr.unnone')
+		    os.system('cd mr.unnone')
+		   os.system('python2 BD11.py')
+elif pil in ["03","3"]:
+	
+print('Username = Mahadi  Password - Hasan')
+	      try: 
+	           os.system('git clone https://github.com/Shuvo-BBHH/bdcluner11')
+		  os.system('cd bdcluner11')
+		  os.system('python2 mahdi11.py')
+		
+elif pil in ["04","4"]:	
+	
+print('Username = Mahadi  Password - Hasan')
+             
+	try: 
+	       os.system('git clone https://github.com/Shuvo-BBHH/bdcluner11')
+		os.system('cd bdcluner11')
+		os.system('python2 Mahadi-new.py')
+elif pil in ["05","5"]:	
+	try: 
+	             os.system('git clone https://github.com/Shuvo-BBHH/mr.unnone')
+	             os.system('cd mr.unnone')
+	   	     os.system('python baba.py')
