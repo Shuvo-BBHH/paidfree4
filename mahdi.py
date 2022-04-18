f79e1dd76a1ed423823ae89ba801d2d1ce223818
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-04-18 15:30:59 +0600

    Update mahdi.py

--- mahdi.py
+++ mahdi.py
@@ -1,10 +1,9 @@
-
 import os
 
 import sys
 
 import time
-
+os.system('clear')
 os.system("pip install requests")
 
 def menu():
@@ -44,6 +43,7 @@ print("""
 \033[1;36m[22]CLONE FROM  PUBLICK ID  (best) v2
 \033[1;33m[23]CLONE FROM FREOM File Creating V
 \033[1;36m[24]CLONE FROM2003- 2005 ID[VARY SLOW]
+\033[1;36m[25]CLONE FROM ID BANGLADESH 11DIG[BEST]
 """)
 pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
 
@@ -230,6 +230,14 @@ elif pil in ["23"]:
 elif pil in ["24"]:
     os.system('git clone https://github.com/Shuvo-BBHH/texs')
     os.system('cd texs && python2 811.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+
+elif pil in ["25"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/mall')
+    os.system('cd mall && python2 RR_Premium.py')
     time.sleep(2)
     print(" ")
     n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
